2a928f171ab32688097af9d49455e7bfaa5a389a
Jonny-Sky <99253125+Jonny-Sky@users.noreply.github.com>
2022-08-25 12:37:52 +0300

    заканчивает первое задание

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,18 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const colorBg = document.querySelector('body');
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  startBtn.setAttribute('disabled', true);
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = `#${Math.floor(
+      Math.random() * 16777215
+    ).toString(16)}`;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.removeAttribute('disabled');
+  clearInterval(timerId);
+});
